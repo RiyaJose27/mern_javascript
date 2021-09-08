1b5f5c0e203903fa25ed37ae6696f07d4d45651e
Riya Jose Pratheeksh <rjosepratheeksh@gmail.com>
2021-09-08 09:46:39 -0500

    mern_javascript

--- string1.js
+++ string1.js
@@ -3,29 +3,52 @@
 //and don't you dare use built in functions for this one!
 //"Snaps! crackles! pops!" -> "Snrackle ops!"
 //scoop dedupe!
+// const dedupe = (str) => {
+//     var charArr = [];
+//     var output = "";
+//     for (var i = str.length-1; i >= 0 ;--i){
+//         if(!find(charArr,str[i])){
+//             charArr.push(str[i]);
+//             output = str[i] + output;
+//         }
+//     }
+//     return output;
+// }
+
+// const find = (arr, letter) =>{
+//     for(var i = 0; i < arr.length; ++i){
+//         if(arr[i] == letter){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+
+// console.log(dedupe("Snaps! crackles! pops!"));
+// console.log(dedupe("Did I shine my shoes today?"));
+// console.log(dedupe("scoop dedupe!"));
+
 const dedupe = (str) => {
-    var charArr = [];
+    var arr = [];
     var output = "";
-    for (var i = str.length-1; i >= 0 ;--i){
-        if(!find(charArr,str[i])){
-            charArr.push(str[i]);
+    count = 0;
+    for (var i = str.length - 1; i >=0; i--){
+        for (var j=0; j < arr.length; j++){
+            if (str[i] === arr[j]){
+                count++;
+            }
+        }
+        if (count === 0){
+            arr.push(str[i]);
             output = str[i] + output;
         }
+        count = 0;
     }
     return output;
-}
-
-const find = (arr, letter) =>{
-    for(var i = 0; i < arr.length; ++i){
-        if(arr[i] == letter){
-            return true;
-        }
-    }
-    return false;
 }
 
 
 console.log(dedupe("Snaps! crackles! pops!"));
 console.log(dedupe("Did I shine my shoes today?"));
 console.log(dedupe("scoop dedupe!"));
-
