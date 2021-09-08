bc8c9a662b60a6bccf97b9cfe4cdcf3a486ea1f8
Riya Jose Pratheeksh <rjosepratheeksh@gmail.com>
2021-09-08 09:50:34 -0500

    mern_javascript

--- string1.js
+++ string1.js
@@ -29,25 +29,48 @@
 // console.log(dedupe("Did I shine my shoes today?"));
 // console.log(dedupe("scoop dedupe!"));
 
+// const dedupe = (str) => {
+//     var arr = [];
+//     var output = "";
+//     count = 0;
+//     for (var i = str.length - 1; i >=0; i--){
+//         for (var j=0; j < arr.length; j++){
+//             if (str[i] === arr[j]){
+//                 count++;
+//             }
+//         }
+//         if (count === 0){
+//             arr.push(str[i]);
+//             output = str[i] + output;
+//         }
+//         count = 0;
+//     }
+//     return output;
+// }
+
+
+// console.log(dedupe("Snaps! crackles! pops!"));
+// console.log(dedupe("Did I shine my shoes today?"));
+// console.log(dedupe("scoop dedupe!"));
+
+
+
+
 const dedupe = (str) => {
-    var arr = [];
-    var output = "";
-    count = 0;
-    for (var i = str.length - 1; i >=0; i--){
-        for (var j=0; j < arr.length; j++){
-            if (str[i] === arr[j]){
-                count++;
+    newString = '';
+    for(let i = 0; i < str.length; i++){
+        let counter = 0;
+        for(let j = i + 1; j < str.length; j++){
+            if(str[i] == str[j]){
+                counter++;
             }
         }
-        if (count === 0){
-            arr.push(str[i]);
-            output = str[i] + output;
+        if(counter == 0){
+            newString += str[i];
         }
-        count = 0;
     }
-    return output;
+    return newString
 }
-
 
 console.log(dedupe("Snaps! crackles! pops!"));
 console.log(dedupe("Did I shine my shoes today?"));
